5a54a4eb92d336825f3bb510175170bb5ec18aa1
bryarpatch36 <bryar.bush36@gmail.com>
2025-02-16 09:38:14 -0600

    Created Asteroid Field and Asteroid Sprite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -10,12 +12,15 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(x, y)
-    
     BLACK = (0, 0, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    AsteroidField()
     clock = pygame.time.Clock()
     dt = 0
 
